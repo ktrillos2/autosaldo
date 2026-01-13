e33cbf02f1c36415a5089296a89ae16332de4060
ktrillos2 <ktrillos2@gmail.com>
2026-01-12 22:29:15 -0500

    Arrelgo de key

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -2,10 +2,9 @@ import { NextResponse } from "next/server"
 import { Resend } from "resend"
 import { EmailTemplate } from "@/components/email-template"
 
-const resend = new Resend(process.env.RESEND_API_KEY)
-
 export async function POST(req: Request) {
     try {
+        const resend = new Resend(process.env.RESEND_API_KEY)
         const { type, data } = await req.json()
 
         const subject = type === 'cotizacion'
